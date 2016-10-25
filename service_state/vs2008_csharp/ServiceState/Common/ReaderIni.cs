45ba8bff54cef2375f9d15d4de170fa0d8f30e4c
Kazunori OYA <terje@oya3.net>
2016-10-25 13:13:01 +0900

    ini読み込みクラス追加

---
+++ service_state/vs2008_csharp/ServiceState/Common/ReaderIni.cs
@@ -0,0 +1,28 @@
+﻿using System;
+// using System.Collections.Generic;
+// using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace ServiceState.Common
+{
+    static class ReaderIni
+    {
+        [DllImport("KERNEL32.DLL")]
+        private static extern int GetPrivateProfileString(
+            string lpApplicationName,
+            string lpKeyName,
+            string lpDefault,
+            StringBuilder lpReturnedstring,
+            int nSize,
+            string lpFileName);
+
+        public static string GetValue(string path, string section, string key, string defaultKey)
+        {
+            StringBuilder sb = new StringBuilder(256);
+            string fullpath = System.IO.Directory.GetCurrentDirectory() + "\\" + path;
+            int res = GetPrivateProfileString(section, key, defaultKey, sb, sb.Capacity, fullpath);
+            return sb.ToString();
+        }
+    }
+}
