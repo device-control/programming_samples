c694afd9ce0216b0094e10667f7aa0913b7f6550
Kazunori OYA <terje@oya3.net>
2016-12-08 23:48:20 +0900

    refactoring

--- service_state/vs2008_csharp/ServiceState/State/SerialTestStateA.cs
+++ service_state/vs2008_csharp/ServiceState/State/SerialTestStateA.cs
@@ -13,28 +13,28 @@ namespace ServiceState.State
 
         public override void EventA(Common.Service service, Common.Event ev)
         {
-            ((Service.SerialTestService)service).SendMessage(1,System.Text.Encoding.ASCII.GetBytes("{ \"eventName\" : \"EventB\" }\0"));
+            ((Service.SerialTestService)service).SendMessage(1,System.Text.Encoding.ASCII.GetBytes("EventB"));
             Console.Write("Serial EventA\n");
             System.Threading.Thread.Sleep(1 * 1000);
         }
         
         public override void EventB(Common.Service service, Common.Event ev)
         {
-            ((Service.SerialTestService)service).SendMessage(0,System.Text.Encoding.ASCII.GetBytes("{ \"eventName\" : \"EventC\" }\0"));
+            ((Service.SerialTestService)service).SendMessage(0,System.Text.Encoding.ASCII.GetBytes("EventC"));
             Console.Write("Serial EventB\n");
             System.Threading.Thread.Sleep(1 * 1000);
         }
 
         public override void EventC(Common.Service service, Common.Event ev)
         {
-            ((Service.SerialTestService)service).SendMessage(1,System.Text.Encoding.ASCII.GetBytes("{ \"eventName\" : \"EventD\" }\0"));
+            ((Service.SerialTestService)service).SendMessage(1,System.Text.Encoding.ASCII.GetBytes("EventD"));
             Console.Write("Serial EventC\n");
             System.Threading.Thread.Sleep(1 * 1000);
         }
         
         public override void EventD(Common.Service service, Common.Event ev)
         {
-            ((Service.SerialTestService)service).SendMessage(0,System.Text.Encoding.ASCII.GetBytes("{ \"eventName\" : \"EventA\" }\0"));
+            ((Service.SerialTestService)service).SendMessage(0,System.Text.Encoding.ASCII.GetBytes("EventA"));
             Console.Write("Serial EventD\n");
             System.Threading.Thread.Sleep(1 * 1000);
         }
