b6822528517b5e23a96817048662acf245136f1c
Kazunori OYA <kazunori_oya@nsware.co.jp>
2019-11-15 15:54:20 +0900

    add timer

---
+++ service_state/vs2008_csharp/ServiceState/Common/TimeoutEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServiceState.Common
+{
+    public class TimeoutEvent: Event
+    {
+        public string TimerName { get; set; }
+        public int SequenceNumber { get; set; }
+        public TimeoutEvent(string name, string timerName, int sequenceNumber) : base(name)
+        {
+            this.Name = name;
+            this.TimerName = timerName;
+            this.SequenceNumber = sequenceNumber;
+        }
+   }
+}
---
+++ service_state/vs2008_csharp/ServiceState/Common/TimerManager.cs
@@ -0,0 +1,135 @@
+﻿using System.Timers;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ServiceState.Common
+{
+    public struct TimerInfo
+    {
+        public string name;
+        public int initial_timeout;
+        public int timeout;
+        public int loopCount;
+        public Service sv;
+        public Event ev;
+
+        public TimerInfo(string name, int timeout, int loopCount, Service sv, Event ev)
+        {
+            this.name = name;
+            this.initial_timeout = this.timeout = timeout;
+            this.loopCount = loopCount;
+            this.sv = sv;
+            this.ev = ev;
+        }
+    }
+
+    class TimerManager
+    {
+        public static readonly int TIMER_CONTINUOUS = -1;
+        private readonly int TIMER_INTERVAL = 200; // 200ms
+        private static readonly TimerManager instance = new TimerManager();
+        private static Mutex m_mutex = new Mutex();
+        private System.Timers.Timer m_timer = new System.Timers.Timer();
+        Dictionary<string, TimerInfo> m_timerInfos = new Dictionary<string, TimerInfo>();
+        private TimerManager()
+        {
+            m_timer.Enabled = true;
+            m_timer.AutoReset = true;
+            m_timer.Interval = TIMER_INTERVAL;
+            m_timer.Elapsed += new ElapsedEventHandler(OnTimerEvent);
+        }
+
+        private void OnTimerEvent(object source, ElapsedEventArgs e)
+        {
+            m_mutex.WaitOne();
+            if (null != m_timer)
+            {
+                List<string> names = new List<string>(m_timerInfos.Keys);
+                foreach (string name in names)
+                {
+                    TimerInfo tmi = m_timerInfos[name];
+                    tmi.timeout -= TIMER_INTERVAL;
+                    m_timerInfos[name] = tmi;
+                    if ( tmi.timeout <= 0 )
+                    {
+                        tmi.timeout = tmi.initial_timeout;
+                        tmi.sv.AddEvent(tmi.ev);
+                        if (tmi.loopCount == TIMER_CONTINUOUS) // 無限リピート
+                        {
+                            m_timerInfos[name] = tmi;
+                            continue;
+                        }
+                        tmi.loopCount -= 1;
+                        if(tmi.loopCount == 0)
+                        {
+                            m_timerInfos.Remove(name);
+                            continue;
+                        }
+                        m_timerInfos[name] = tmi;
+                    }
+                }
+            }
+            m_mutex.ReleaseMutex();
+        }
+
+        public bool Add(string name, int timeout, int loopCount, Service sv, Event ev)
+        {
+            bool result = true;
+            m_mutex.WaitOne();
+            if( null != m_timer )
+            {
+                if (m_timerInfos.ContainsKey(name))
+                {
+                    result = false; // 登録済み
+                }
+                else
+                {
+                    m_timerInfos.Add(name, new TimerInfo(name, timeout, loopCount, sv, ev));
+                }
+            }
+            m_mutex.ReleaseMutex();
+            return result;
+        }
+
+        public bool Remove(string name)
+        {
+            bool result = true;
+            m_mutex.WaitOne();
+
+            if (null != m_timer)
+            {
+                if (m_timerInfos.ContainsKey(name))
+                {
+                    m_timerInfos.Remove(name);
+                }
+                else
+                {
+                    result = false;   
+                }
+            }
+            m_mutex.ReleaseMutex();
+            return result;
+        }
+
+        public void Stop()
+        {
+            m_mutex.WaitOne();
+
+            m_timer.Stop();
+            m_timer = null;
+            m_timerInfos.Clear();
+            m_mutex.Close();
+
+            m_mutex.ReleaseMutex();
+
+        }
+
+        public static TimerManager Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
